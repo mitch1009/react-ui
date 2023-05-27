84ac0dd9c218363814d2b511755d58d763226602
Mitch Chanza <hello@mitch.guru>
2023-05-27 02:06:15 +0200

    feat: update auth component

--- src/components/Auth/AuthComponent.tsx
+++ src/components/Auth/AuthComponent.tsx
@@ -14,7 +14,6 @@ export const AuthComponent =()=>{
               Sign in to your account
             </h2>
           </div>
-  
           <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-[480px]">
             <div className="bg-white px-6 py-12 shadow sm:rounded-lg sm:px-12">
               <form className="space-y-6" action="#" method="POST">
